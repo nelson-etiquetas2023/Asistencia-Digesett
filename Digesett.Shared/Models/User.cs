f6fa300139b47abd31835fce7c3996d4e2950e07
Nelson Pino <devsoftware.etiquetas@gmail.com>
2024-08-10 10:20:58 -0400

    commit inicial

---
+++ Digesett.Shared/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Digesett.Shared.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = null!;
+        [Required]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string TypeUser { get; set; } = null!;
+        [Required]
+        public string Cargo { get; set; } = null!;
+        public string Departament { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public bool Active { get; set; }
+    }
+}

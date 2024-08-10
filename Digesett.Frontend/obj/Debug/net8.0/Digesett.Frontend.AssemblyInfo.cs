43dc67422c26adbee29c7934c340cce2e7840e53
Nelson Pino <devsoftware.etiquetas@gmail.com>
2024-08-10 17:29:06 -0400

    avances en la ui de create usuario

--- Digesett.Frontend/obj/Debug/net8.0/Digesett.Frontend.AssemblyInfo.cs
+++ Digesett.Frontend/obj/Debug/net8.0/Digesett.Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Digesett.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ead6354897de2eb0ec56380d7281a3075962727d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f756bc7a14a11b2c241217c704123d8c1f182d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Digesett.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Digesett.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

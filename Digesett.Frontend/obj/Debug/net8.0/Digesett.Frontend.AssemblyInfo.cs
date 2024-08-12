1caec07473ce10513a229bcdcd68ed88bb6fd408
Nelson Pino <devsoftware.etiquetas@gmail.com>
2024-08-12 10:35:09 -0400

    integracion con sweet alert 2

--- Digesett.Frontend/obj/Debug/net8.0/Digesett.Frontend.AssemblyInfo.cs
+++ Digesett.Frontend/obj/Debug/net8.0/Digesett.Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Digesett.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f756bc7a14a11b2c241217c704123d8c1f182d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43dc67422c26adbee29c7934c340cce2e7840e53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Digesett.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Digesett.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

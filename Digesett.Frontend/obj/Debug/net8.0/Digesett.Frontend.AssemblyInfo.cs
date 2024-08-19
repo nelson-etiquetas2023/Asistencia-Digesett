9304d4afde5f8eea0babde12e7730846456be7de
Nelson Pino <devsoftware.etiquetas@gmail.com>
2024-08-19 12:45:54 -0400

    entrega de la funcionalidad de borrar usuarios

--- Digesett.Frontend/obj/Debug/net8.0/Digesett.Frontend.AssemblyInfo.cs
+++ Digesett.Frontend/obj/Debug/net8.0/Digesett.Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Digesett.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f17dc6a02ff18b987fe1d1481bb5bed7a4025d0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db004a88caea5bd6c159214021b460ff89f1e072")]
 [assembly: System.Reflection.AssemblyProductAttribute("Digesett.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Digesett.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

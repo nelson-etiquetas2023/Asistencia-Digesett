d61c74112de96ceca6c45685787ec4416265d4d0
Nelson Pino <devsoftware.etiquetas@gmail.com>
2024-08-28 16:39:41 -0400

    agregar validacion de usuarios.

--- Digesett.Server/obj/Debug/net8.0/Digesett.Server.AssemblyInfo.cs
+++ Digesett.Server/obj/Debug/net8.0/Digesett.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Digesett.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e24d355709e4b2f82e558a91ebdb99f7e795d94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1daecc68a46e4f567202652260f67c2f0477ca0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Digesett.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Digesett.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Digesett.Shared/Models/User.cs
+++ Digesett.Shared/Models/User.cs
@@ -6,15 +6,15 @@ namespace Digesett.Shared.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="El Nombre del usuario es requerido")]
         public string Name { get; set; } = null!;
-        [Required]
+        [Required(ErrorMessage ="El email es un campo requerido")]
         public string Email { get; set; } = null!;
-        [Required]
+        [Required(ErrorMessage ="El campo Contraseña es requerido")]
         public string Password { get; set; } = null!;
-        [Required]
+        [Required(ErrorMessage = "El campo tipo de usuario es requerido")]
         public string TypeUser { get; set; } = null!;
-        [Required]
+        [Required(ErrorMessage = "El departamento es un campo requerido")]
         public string Departament { get; set; } = null!;
         public string Cargo { get; set; } = null!;
         public string Phone { get; set; } = null!;
